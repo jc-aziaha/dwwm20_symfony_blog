d621c6911cda75bc713f8991e9c766a08830d6d9
Jean-Claude AZIAHA <aziaha.formations@gmail.com>
2025-04-30 16:14:20 +0200

    feat: Insertion d'une nouvelle catégorie en place

--- src/Controller/Admin/Category/CategoryController.php
+++ src/Controller/Admin/Category/CategoryController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin\Category;
 
 use App\Entity\Category;
 use App\Form\AdminCategoryFormType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin')]
 final class CategoryController extends AbstractController
@@ -17,12 +20,27 @@ final class CategoryController extends AbstractController
         return $this->render('pages/admin/category/index.html.twig');
     }
 
-    #[Route('/category/create', name: 'app_admin_category_create', methods: ['GET'])]
-    public function create(): Response
+    #[Route('/category/create', name: 'app_admin_category_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $category = new Category();
 
         $form = $this->createForm(AdminCategoryFormType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() )
+        {
+            $category->setCreatedAt(new DateTimeImmutable());
+            $category->setUpdatedAt(new DateTimeImmutable());
+
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', "La catégorie {$category->getName()} a été ajoutée avec succès.");
+
+            return $this->redirectToRoute('app_admin_category_index');
+        }
 
         return $this->render("pages/admin/category/create.html.twig", [
             "form" => $form->createView()
